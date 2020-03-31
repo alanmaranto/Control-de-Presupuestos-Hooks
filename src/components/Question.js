7fcbde657c6de071cdeffa2c965425ee638ddf2e
alan <alanarostegui@hotmail.com>
2020-03-31 16:27:52 -0600

    feature(question): onchange function and onSubmit

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Question = props => {
+const Question = ({
+    onChangeBudget
+}) => {
   return (
     <div>
       <h2>Coloca tu presupuesto</h2>
@@ -10,6 +12,8 @@ const Question = props => {
           type="text"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={onChangeBudget}
+        //   onChange={ e => setQuantity(parseInt(e.target.value, 10))}
         />
         <input
           type="submit"
--- src/containers/QuestionContainer.js
+++ src/containers/QuestionContainer.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Question from "../components/Question";
 
 const QuestionContainer = () => {
+  const [quantity, setQuantity] = useState(0);
+
+  const onChangeBudget = e => {
+    setQuantity(parseInt(e.target.value, 10));
+  };
+
+  const onSubmitBudget = e => {
+    e.preventDefault();
+
+    // Validate
+
+    // Aftet validate
+  };
+
   return (
-    <Question />
+    <Question onChangeBudget={onChangeBudget} onSubmitBudget={onSubmitBudget} />
   );
 };
 
