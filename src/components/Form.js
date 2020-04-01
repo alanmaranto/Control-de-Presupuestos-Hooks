cd8aa4f0ab8fda8105c31e9dc7d0c38234ba98e6
alan <alanarostegui@hotmail.com>
2020-03-31 21:16:26 -0600

    feature(form container): add state to expenses

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Form = props => {
+const Form = ({ amount, title, onChange }) => {
   return (
     <div>
       <form>
         <h2>Agrega tus datos aquí</h2>
         <div className="campo">
-          <label>Nonbre Gasto</label>
+          <label>Nombre Gasto</label>
           <input
             type="text"
             className="u-full-width"
             placeholder="Ej. Transporte"
+            value={title}
+            name="title"
+            onChange={onChange('title')}
           />
         </div>
         <div className="campo">
           <label>Cantidad Gasto</label>
-          <input type="number" className="u-full-width" placeholder="Ej. 300" />
+          <input
+            type="number"
+            className="u-full-width"
+            placeholder="Ej. 300"
+            value={amount}
+            name="amount"
+            onChange={onChange('amount')}
+          />
         </div>
         <input
           type="submit"
--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "../components/Form";
 
+const initialState = {
+  title: "",
+  amount: 0
+};
+
 const FormContainer = () => {
-  return <Form />;
+  const [values, setValues] = useState(initialState);
+
+  const { title, amount } = values;
+
+  const onSubmit = e => {
+      e.preventDefault();
+
+      // validate
+
+      //Expense
+
+      // Pass to main component
+
+      //Reset form
+  }
+
+  const onChange = name => e => {
+    setValues({
+      ...values,
+      [name]: e.target.value
+    });
+  };
+
+  return <Form title={title} amount={amount} onChange={onChange} />;
 };
 
 export default FormContainer;
